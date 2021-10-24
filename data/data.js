9d797d1a80278d2e0385e4cbacf43daa8de4c5a1
Alex Beltran <pebeltranhe@gmail.com>
2021-10-24 13:31:22 -0500

    Primera version, hasta ahora se creo la interfaz, un archuvo con las preguntas y la clase de preguntas

---
+++ data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    question: "¿cuanto es 1+1?",
+    choices: ["7", "5", "10", "2"],
+    answer: 3,
+  },
+  {
+    question: "¿de que color es el caballo blanco de simon bolivar?",
+    choices: ["verde", "rojo", "azul", "blanco"],
+    answer: 3,
+  },
+  {
+    question: "¿si tengo tres peces, y los echo al agua, cuantos se ahogan?",
+    choices: ["todos","2 peces","1 pez","ninguno, los peces no se ahogan en el agua"],
+    answer: 3,
+  },
+];
